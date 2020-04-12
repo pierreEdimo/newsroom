fe4da6db4aea3a52e6bc6f73c5a66c6af45a150e
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-12 16:34:39 +0200

    i added the configuration

--- findaDoctor/Startup.cs
+++ findaDoctor/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using findaDoctor.DBcontext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore; 
 
 namespace findaDoctor
 {
@@ -25,6 +27,8 @@ namespace findaDoctor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("FinDaDoctorDatabase"); 
+            services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(connection));
             services.AddControllers();
         }
 
