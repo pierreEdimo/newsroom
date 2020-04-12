0e70d751d37df2ce6387e02d74ddee7234c2869c
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-12 16:34:18 +0200

    i added the model

---
+++ findaDoctor/model/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace findaDoctor.model
+{
+    public class Doctor
+    {
+        public int Id { get; set;  }
+        public string name { get; set;  }
+        public string category { get; set; }
+        public string description { get; set;  }
+        public DateTime createdAt { get; set; } = DateTime.Now; 
+        public string city { get; set;  }
+        public string country { get; set;  }
+        public string address { get; set;  }
+        public int poBox { get; set;  }
+        public string email { get; set;  }
+        public string teleFonNumber { get; set;  }
+
+    }
+}
