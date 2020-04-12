2c1ed58390194c38aa404a15adfd10339f9fa5b9
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-12 16:33:33 +0200

    i added the database context

---
+++ findaDoctor/DBcontext/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using findaDoctor.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace findaDoctor.DBcontext
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options) { }
+
+        public DbSet<Doctor> Doctors { get; set;  }
+    }
+}
