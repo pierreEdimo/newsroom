79c63987009388fbdb35b12a6957f5430464877e
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-13 22:54:19 +0200

    config datas

--- DBContext/BuilderExtensions.cs
+++ DBContext/BuilderExtensions.cs
@@ -14,11 +14,11 @@ namespace findaDoctor.DBContext
             );
 
             modelBuilder.Entity<Doctor>().HasData(
-                new Doctor { Id = 1, name = "Aurelius", categoryId = 1, description = "doctor", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
-                new Doctor { Id = 2, name = "Aurelius", categoryId = 1, description = "doctor", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
-                new Doctor { Id = 3, name = "Aurelius", categoryId = 2, description = "doctor", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
-                new Doctor { Id = 4, name = "Aurelius", categoryId = 2, description = "doctor", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
-                new Doctor { Id = 5, name = "Aurelius", categoryId = 3, description = "doctor", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" }
+                new Doctor { Id = 1, name = "Aurelius", categoryId = 1, specialisation = "psychiater", description = "specialised in psychosomatischen Problem", location = "am Kleegarten 17", city = "Frankfurt", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
+                new Doctor { Id = 2, name = "Aurelius", categoryId = 1, specialisation = "ophtamologue", description = "specialised in psychosomatischen Problem", location = "am Kleegarten 17", city = "Frankfurt", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
+                new Doctor { Id = 3, name = "Aurelius", categoryId = 2, specialisation = "dermatologue", description = "specialised in psychosomatischen Problem", location = "am Kleegarten 17", city = "Frankfurt", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
+                new Doctor { Id = 4, name = "Aurelius", categoryId = 2, specialisation = "psychiater", description = "specialised in psychosomatischen Problem", location = "am Kleegarten 17", city = "Fulda", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" },
+                new Doctor { Id = 5, name = "Aurelius", categoryId = 3, specialisation = "dermatologue", description = "specialised in psychosomatischen Problem", location = "am Kleegarten 17", city = "Frankfurt", country = "Deutschland", email = "pedimonkoe@yahoo.com", number = "+15758516374" }
             );
         }
     }
--- DTO/DoctorsDTo.cs
+++ DTO/DoctorsDTo.cs
@@ -9,6 +9,7 @@ namespace findaDoctor.DTO
     {
         public int Id { get; set; }
         public string name { get; set; }
+        public string specialisation { get; set; }
         public string description { get; set; }
         public string location { get; set; }
         public string city { get; set; }
