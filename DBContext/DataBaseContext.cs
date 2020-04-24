9d724f1ebe1dfd613c08ee02e0b2b7d83217823f
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-24 21:52:57 +0200

    i set the context for Author , Theme, and User

--- DBContext/DataBaseContext.cs
+++ DBContext/DataBaseContext.cs
@@ -13,11 +13,20 @@ namespace findaDoctor.DBContext
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Theme>().HasMany(c => c.Articles).WithOne(a => a.Theme).HasForeignKey(a => a.themeId);
+
+            modelBuilder.Entity<Author>().HasMany(c => c.Articles).WithOne(a => a.Author).HasForeignKey(a => a.authorId);
+
+            modelBuilder.Entity<User>().HasMany(f => f.Favourites);
+
 
             modelBuilder.Seed();
         }
 
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Article> Articles { get; set; }
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
