6a211f51dee0e1d6453f12664c2a40b6d2c78b8a
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-25 22:29:24 +0200

    validation attribut

--- Model/Doctor.cs
+++ Model/Doctor.cs
@@ -1,22 +1,44 @@
 using System;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace findaDoctor.Model
 {
     public class Doctor
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string name { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string specialisation { get; set; }
+        [Required]
+        [MaxLength(8000)]
         public string description { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string poBox { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string location { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string city { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string country { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string email { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string number { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string searchWord { get; set; }
+        [Required]
         public string imageUrl { get; set; }
         public DateTime opening { get; set; }
         public DateTime closing { get; set; }
