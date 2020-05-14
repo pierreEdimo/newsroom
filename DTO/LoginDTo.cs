ebe401d77c353bdadd951d88d1620acc2da38c66
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-14 11:43:55 +0200

    i added the login dto

---
+++ DTO/LoginDTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findaDoctor.DTO
+{
+    public class LoginDTo
+    {
+        public string Email { get; set;  }
+        public string Password { get; set; }
+    }
+}
