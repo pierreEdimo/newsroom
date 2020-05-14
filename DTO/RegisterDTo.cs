0f189f15acd1db56c2a543f5c934b2781cc74fd5
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-14 11:44:17 +0200

    i added the RegisterDTO

---
+++ DTO/RegisterDTo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findaDoctor.DTO
+{
+    public class RegisterDTo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string UserName { get; set; }
+        public string location { get; set;  }
+        public string city { get; set;  }
+        public string country { get; set;  }
+        public string poBox { get; set;  }
+        public string passWord { get; set;  }
+        public string PhoneNumber { get; set;  }
+        public string Email { get; set;  }
+        
+    }
+}
