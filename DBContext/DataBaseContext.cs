eaf8b4a4d5de300bbe965cafb8b4e48769d9e180
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-14 11:45:39 +0200

    i configured relationships withFluent Api

--- DBContext/DataBaseContext.cs
+++ DBContext/DataBaseContext.cs
@@ -1,21 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 using findaDoctor.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace findaDoctor.DBContext
 {
 
 
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : IdentityDbContext
 
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
+        public DatabaseContext()
+        {
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder); 
+
             modelBuilder.Entity<Theme>().HasMany(c => c.Articles).WithOne(a => a.Theme).HasForeignKey(a => a.themeId);
 
-            modelBuilder.Entity<User>().HasMany(f => f.Favourites);
+            modelBuilder.Entity<FavoriteArticle>().HasKey(f => new { f.articleId, f.userId });
+
+            modelBuilder.Entity<FavoriteDoctor>().HasKey(f => new { f.doctorId, f.userId }); 
+
+            modelBuilder.Entity<FavoriteArticle>().HasOne(c => c.UserEntity).WithMany(a => a.FavoriteArticles).HasForeignKey(a => a.userId);
+
+            modelBuilder.Entity<FavoriteArticle>().HasOne(c => c.Article).WithMany(a => a.FavoriteArticleRef).HasForeignKey(a => a.articleId);
+
+            modelBuilder.Entity<FavoriteDoctor>().HasOne(c => c.UserPatient).WithMany(a => a.FavoriteDoctors).HasForeignKey(a => a.userId);
+
+            modelBuilder.Entity<FavoriteDoctor>().HasOne(c => c.Doctor).WithMany(a => a.FavoriteDoctorRef).HasForeignKey(a => a.doctorId);
+
+            modelBuilder.Entity<UserEntity>().HasMany(c => c.FavoriteArticles);
+
+            modelBuilder.Entity<UserEntity>().HasMany(c => c.FavoriteDoctors);
+
+           
 
             modelBuilder.Entity<Article>().HasOne(f => f.Theme).WithMany(a => a.Articles);
 
@@ -25,6 +47,8 @@ namespace findaDoctor.DBContext
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Theme> Themes { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<FavoriteDoctor> FavoriteDoctors { get; set; }
+        public DbSet<FavoriteArticle> FavortieArticles { get; set;  }
+       
     }
 }
