2e307f00bd921df13a66920de85d53fd145a8d90
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-14 11:46:26 +0200

    User Model

---
+++ Model/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace findaDoctor.Model
+{
+    public class UserEntity: IdentityUser
+    {
+        public string firstName { get; set;  }
+        public string lastName { get; set;  }
+        public string location { get; set;  }
+        public string city { get; set;  }
+        public string country { get; set;  }
+        public string poBox { get; set;  }
+        public virtual List<FavoriteDoctor> FavoriteDoctors { get; set; }
+        public virtual List<FavoriteArticle> FavoriteArticles { get; set;  }
+        
+    }
+}
