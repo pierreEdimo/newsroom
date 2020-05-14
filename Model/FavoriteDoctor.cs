453abeeca424a4d1d4384ff54731ed713e8fd8b7
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-14 11:46:44 +0200

    FavoriteDoctorModel

---
+++ Model/FavoriteDoctor.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findaDoctor.Model
+{
+    public class FavoriteDoctor
+    {
+        public int Id { get; set;  }
+        public string userId { get; set; }
+        public int doctorId { get; set;  }
+        [JsonIgnore]
+        public virtual Doctor Doctor { get; set;  }
+        [JsonIgnore]
+        public virtual UserEntity UserPatient { get; set;  }
+    }
+}
