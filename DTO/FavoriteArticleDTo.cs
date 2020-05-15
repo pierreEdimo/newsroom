6a96c7e342ea55a1f964c10102d31dcd6ce5b371
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-15 11:31:44 +0200

    i added the favorite article dto

---
+++ DTO/FavoriteArticleDTo.cs
@@ -0,0 +1,13 @@
+﻿using findaDoctor.Model; 
+
+namespace findaDoctor.DTO
+{
+    public class FavoriteArticleDTo
+    {
+        public int Id { get; set; }
+        public string userId { get; set; }
+        public int articleId { get; set; }
+        public virtual Article Article { get; set; }
+        public virtual UserEntity UserEntity { get; set; }
+    }
+}
