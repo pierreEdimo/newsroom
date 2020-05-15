f07ff020c4980d0e8a0ec9e8f5d883e47cc91b17
Pierre Edímo <pedimonkoe@yahoo.com>
2020-05-15 11:32:28 +0200

    i created the favorite doctor dto

---
+++ DTO/FavoriteDoctorDTo.cs
@@ -0,0 +1,16 @@
+﻿using findaDoctor.Model;
+using System.Text.Json.Serialization;
+
+namespace findaDoctor.DTO
+{
+    public class FavoriteDoctorDTo
+    {
+        
+        public int Id { get; set; }
+        public string userId { get; set; }
+        public int doctorId { get; set; }
+        public virtual Doctor Doctor { get; set; }
+        public virtual UserEntity UserPatient { get; set; }
+
+    }
+}
