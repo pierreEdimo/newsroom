81762419fded72d1a595286dac0935574ee9f15f
Pierre Edímo <pedimonkoe@yahoo.com>
2020-06-30 21:32:36 +0200

    i changed some validations issues

--- Model/UserEntity.cs
+++ Model/UserEntity.cs
@@ -6,30 +6,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace findaDoctor.Model
 {
-    public class UserEntity: IdentityUser
+    public class UserEntity : IdentityUser
     {
-        [Required]
+
         [MaxLength(50)]
         [DisplayName("Firstname")]
-        public string firstName { get; set;  }
-        [Required]
+        public string firstName { get; set; }
         [MaxLength(50)]
         [DisplayName("Lastname")]
-        public string lastName { get; set;  }
+        public string lastName { get; set; }
         [MaxLength(255)]
         [DisplayName("Current Living address")]
-        public string location { get; set;  }
+        public string location { get; set; }
         [MaxLength(255)]
         [DisplayName("Current City")]
-        public string city { get; set;  }
+        public string city { get; set; }
         [MaxLength(255)]
         [DisplayName("Current living Country")]
-        public string country { get; set;  }
+        public string country { get; set; }
         [MaxLength(50)]
         [DisplayName("Postal Box")]
-        public string poBox { get; set;  }
+        public string poBox { get; set; }
         public virtual List<FavoriteDoctor> FavoriteDoctors { get; set; }
-        public virtual List<FavoriteArticle> FavoriteArticles { get; set;  }
-        
+        public virtual List<FavoriteArticle> FavoriteArticles { get; set; }
+
     }
 }
