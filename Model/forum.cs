6c43dc6e5303af7834354f9a8ccfc9a744097043
Pierre Edímo <pedimonkoe@yahoo.com>
2020-07-15 12:57:58 +0200

    i added the forum functionality

---
+++ Model/forum.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace newsroom.Model
+{
+    public class Forum
+    {
+        [Key]
+        public int Id { get; set; }
+        public string uid { get; set; }
+        [Required]
+        public String title { get; set; }
+        [JsonIgnore]
+        public virtual List<Comments> Comments { get; set; }
+        [JsonIgnore]
+        public virtual UserEntity Author { get; set; }
+        public DateTime createdAt { get; set; } = DateTime.Now;
+
+    }
+}
