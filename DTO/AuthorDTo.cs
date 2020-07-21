af9b3f9eeecb15719e07a348e0fd27bc02e29802
Pierre Edímo <pedimonkoe@yahoo.com>
2020-07-21 23:04:43 +0200

    i added the author model

---
+++ DTO/AuthorDTo.cs
@@ -0,0 +1,22 @@
+﻿using newsroom.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newsroom.DTO
+{
+    public class AuthorDTo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String name { get; set; }
+        [Required]
+        public String biography { get; set; }
+        [Required]
+        public String imageUrl { get; set; }
+        public virtual List<Article> Articles { get; set; }
+    }
+}
---
+++ Model/Author.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newsroom.Model
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set;  }
+        [Required]
+        public String name { get; set;  }
+        [Required]
+        public String biography { get; set; }
+        [Required]
+        public String imageUrl { get; set;  }
+        [JsonIgnore]
+        public virtual List<Article> Articles { get; set;  }
+    }
+}
