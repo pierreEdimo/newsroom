c7d144912464e0b2d55c35d1d4b9b26c635457e1
pierre edimo <pierredimo@live.com>
2021-01-26 20:37:52 +0100

    Add a new article

--- DBContext/BuilderExtensions.cs
+++ DBContext/BuilderExtensions.cs
@@ -11,14 +11,15 @@ namespace newsroom.DBContext
 
             modelBuilder.Entity<Theme>().HasData(
                 new Theme { Id = 1, name = "sport", imageUrl = "https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" },
-                new Theme { Id = 2, name = "Lifestyle", imageUrl = "https://images.unsplash.com/photo-1546548970-71785318a17b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }
+                new Theme { Id = 2, name = "Lifestyle", imageUrl = "https://images.unsplash.com/photo-1546548970-71785318a17b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" },
+                new Theme { Id = 3, name="Tech & It", imageUrl = "https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" }
             );
 
            
 
 
             modelBuilder.Entity<Author>().HasData(
-                new Author { Id = 1, name = "Pierre Edimo", imageUrl = "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60", biography = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr," }
+                new Author { Id = 1, name = "Pierre Edimo", biography = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr," }
               
                 );
 
@@ -49,7 +50,33 @@ namespace newsroom.DBContext
 
                             
 
-              }
+              },
+                 new Article
+                 {
+                     Id = 2,
+                     authorId = 1,
+                     imageUrl = "https://images.unsplash.com/photo-1571987502227-9231b837d92a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+                     title = "Tesla is the highest publicly valuated car manufacturer, what does it mean for the germans car manufacturers",
+                     themeId = 3,
+                     content = "**2020 has been a difficult year , both mentaly and financialy, but as it is nearing its end, i have decided to share my experience to the world**     " +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "my name is Pierre Patrice Emmanuel Edimo Nkoe , i study Computer Science in Germany , where i live since the last 8 years. I came here i was 18 years old" +
+                            "from Cameroon. It has been a humbling experience for me. There were good moments , there were bas moments , i learn a lot from this Country. It is an experience" +
+                            "i want to share with people all around the world." +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "i look forward to share more of my experience and my Interest with have a nice day , and a nice fun reading my articles" +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            " for interested you can [contact me](pedimonkoe@yahoo.com)"
+
+
+
+                 }
             );
 
 
