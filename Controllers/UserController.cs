85b45041743db5275e9b751c3cc2f281e08fc98d
pierreEdimo <pedimonkoe@yahoo.com>
2021-07-06 00:42:16 +0200

    Add UserToken and update Usercontroller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,7 +2,6 @@
 using newsroom.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
@@ -56,31 +55,24 @@ namespace newsroom.Controllers
         }
 
         [HttpPost]
-        public async Task<object> updateEmail([FromBody] UpdateEmailDTO emailDTO )
+        public async Task<ActionResult<UserToken>> updateEmail([FromBody] UpdateEmailDTO emailDTO )
         {
             var email = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;;
 
-            var user = await _userManager.FindByEmailAsync(email); 
+            var user = await _userManager.FindByEmailAsync(email);
 
-            if(user == null)
-            {
-                return NotFound(); 
-            }
+            if (user == null) return NotFound(); 
 
             user.Email = emailDTO.Email;
 
             var result = await _userManager.UpdateAsync(user);
 
-            if (result.Succeeded)
-            {
-                
-                var loggedUser = await _userManager.FindByEmailAsync(emailDTO.Email);
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
-                return GenerateJwtToken(loggedUser.Email, loggedUser);
-            }
+            var loggedUser = await _userManager.FindByEmailAsync(emailDTO.Email);
 
-            return BadRequest(result.Errors);
-        }
+            return GenerateJwtToken(loggedUser.Email, loggedUser); 
+         }
 
         [AllowAnonymous]
         [HttpGet(Name = nameof(GetAllUsers))]
@@ -96,51 +88,38 @@ namespace newsroom.Controllers
 
         [AllowAnonymous]
         [HttpPost]
-        public async Task<object> Login([FromBody] LoginDTo modelLogin)
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginDTo modelLogin)
         {
             var user = await _userManager.FindByEmailAsync(modelLogin.email);
 
-            if (user != null)
-            {
-                var signInResult = await _signInManager.PasswordSignInAsync(user, modelLogin.password, false, false);
+            if (user == null) return NotFound();
 
-                if (signInResult.Succeeded)
-                {
-                    var loggedUser = _userManager.Users.SingleOrDefault(u => u.Email == modelLogin.email);
+            var result = await _signInManager.PasswordSignInAsync(user, modelLogin.password, false, false);
 
-                    return GenerateJwtToken(modelLogin.email, loggedUser);
-                }
-            }
+            if (!result.Succeeded) return BadRequest("Invalid Login");
 
-            throw new ApplicationException("UNKNOWN_ERROR");
+            return GenerateJwtToken(modelLogin.email, user); 
         }
 
         [AllowAnonymous]
         [HttpPost]
-        public async Task<object> Register([FromBody] RegisterDTo model)
+        public async Task<ActionResult<UserToken>> Register([FromBody] RegisterDTo model)
         {
             var user = new UserEntity
             {
-
                 UserName = model.UserName,
                 Email = model.Email,
-                
-
             };
 
             var result = await _userManager.CreateAsync(user, model.passWord);
 
-            if (result.Succeeded)
-            {
-                await _signInManager.PasswordSignInAsync(user, model.passWord, false, false);
-                return GenerateJwtToken(model.Email, user);
-            }
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
-            return BadRequest(result.Errors);
+            return GenerateJwtToken(model.Email, user); 
         }
 
 
-        private object GenerateJwtToken(string email, UserEntity user)
+        private UserToken GenerateJwtToken(string email, UserEntity user)
         {
             var claims = new List<Claim> {
                 new Claim(JwtRegisteredClaimNames.Sub, email),
@@ -157,8 +136,33 @@ namespace newsroom.Controllers
                  signingCredentials: credentials
                  );
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            var loggedUserDto = _mapper.Map<UserDTO>(user);
 
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                UserDto = loggedUserDto
+            }; 
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult<UserToken>> ForgotPassWord(LoginDTo login)
+        {
+            var user = await _userManager.FindByEmailAsync(login.email);
+
+            if (user == null) return NotFound();
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            var result = await _userManager.ResetPasswordAsync(user, token, login.password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            await _signInManager.PasswordSignInAsync(user, login.password, false, false);
+
+            return GenerateJwtToken(login.email, user); 
         }
 
     }
---
+++ DTO/UserToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace newsroom.DTO
+{
+    public class UserToken
+    {
+        public String Token { get; set;  }
+        public UserDTO UserDto { get; set;  }
+    }
+}
