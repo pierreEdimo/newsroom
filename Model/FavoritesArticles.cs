8dc9dcb7eb71cca53ad59fd0a1df16b0f92e9bba
pierreEdimo <pedimonkoe@yahoo.com>
2021-10-03 19:28:44 +0200

    delete a useless data

--- Model/FavoritesArticles.cs
+++ Model/FavoritesArticles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace newsroom.Model
 {
@@ -10,9 +11,11 @@ namespace newsroom.Model
     {
         [Required]
         public int ArticleId { get; set;  }
+        [JsonIgnore]
         public Article Article { get; set;  }
         [Required]
         public String OwnerId { get; set;  }
+        [JsonIgnore]
         public UserEntity User { get; set;  }
     }
 }
