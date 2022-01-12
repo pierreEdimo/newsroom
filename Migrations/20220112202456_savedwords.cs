c2b78ac17aa1f1affafb9d6e337294f7e199c34a
pierreEdimo <pedimonkoe@yahoo.com>
2022-01-12 21:27:53 +0100

    update Migration

---
+++ Migrations/20220112202456_savedwords.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace newsroom.Migrations
+{
+    public partial class savedwords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeyWords");
+
+            migrationBuilder.CreateTable(
+                name: "SavedWords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    Word = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedWords", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 1, 12, 21, 24, 55, 151, DateTimeKind.Local).AddTicks(8942));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 1, 12, 21, 24, 55, 157, DateTimeKind.Local).AddTicks(2317));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 1, 12, 21, 24, 55, 157, DateTimeKind.Local).AddTicks(2364));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2022, 1, 12, 21, 24, 55, 157, DateTimeKind.Local).AddTicks(2392));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedWords");
+
+            migrationBuilder.CreateTable(
+                name: "KeyWords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    Word = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyWords", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2021, 12, 18, 14, 11, 52, 810, DateTimeKind.Local).AddTicks(8501));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2021, 12, 18, 14, 11, 52, 814, DateTimeKind.Local).AddTicks(8549));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2021, 12, 18, 14, 11, 52, 814, DateTimeKind.Local).AddTicks(8597));
+
+            migrationBuilder.UpdateData(
+                table: "Articles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2021, 12, 18, 14, 11, 52, 814, DateTimeKind.Local).AddTicks(8604));
+        }
+    }
+}
